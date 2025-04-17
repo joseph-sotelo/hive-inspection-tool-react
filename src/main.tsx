24657f5d15134509beeecd5a72401448d0658458
Joseph Sotelo <josephhsotelo@gmail.com>
2025-04-17 00:28:25 -0700

    create basic map

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import { defineCustomElements } from "@arcgis/map-components/dist/loader";
+defineCustomElements();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
