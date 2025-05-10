643f875632cf688bfe1b7c0bd585c57511375a42
Joseph Sotelo <josephhsotelo@gmail.com>
2025-05-09 17:30:56 -0700

    style mobile sheet component

--- src/components/mobile-sheet.tsx
+++ src/components/mobile-sheet.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+import { ChevronDown } from "lucide-react";
+
 import clsx from "clsx";
 
 type MobileSheetProps = {
@@ -12,13 +14,15 @@ export default function MobileSheet({props}: {props: MobileSheetProps}) {
   const [isOpen, setIsOpen] = useState(true);
 
   return (
-    <div className={clsx("rounded-xl w-full transition-all duration-300 h-9/10 absolute z-10 bg-white", {"top-1/10": isOpen, "top-9/10": !isOpen})}>
-      <div id="peek" className="p-padding h-12 flex">
+    <div className={clsx("p-6 rounded-xl w-full transition-all duration-400 h-9/10 absolute z-10 bg-white", {"top-1/10": isOpen, "top-9/10": !isOpen})}>
+      <div id="peek" className="p-padding h-12 flex justify-between">
         <div>
-          {props.F_title}
+          <h4>
+            {props.F_title}
+          </h4>
         </div>
         <div onClick={() => (setIsOpen(!isOpen))}>
-          close
+          <ChevronDown />
         </div>
       </div>
       <div id="sheet-body">
